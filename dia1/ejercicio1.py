749a3a35598a022e6030f8abe9bb0b82b9a99dfa
Camila Garrido <camila.t.garrido@gmail.com>
2024-03-07 21:13:33 -0300

    se crean archivos

---
+++ dia1/ejercicio1.py
@@ -0,0 +1,9 @@
+#comentario de una sola linea
+"""
+comentario
+de mas 
+de una
+linea
+"""
+
+print("Hola g8")
---
+++ dia1/mi_primer_programa.py
@@ -0,0 +1,12 @@
+#string-> cadena de caracteres  (se escribe entre comillas)
+nombre = "Camila"
+apellido1="Garrido"
+apellido2="Barrientos"
+edad="25"
+#Python no permite sumar letras con numeros
+print("****** Informacion *********")
+print("**      "+nombre+"            **")
+print("**      "+apellido1+"             **")
+print("**      "+apellido2+"             **")
+print("**      "+edad+"                 **")
+print("****************************+**")
