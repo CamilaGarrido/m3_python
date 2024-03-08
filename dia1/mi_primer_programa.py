d2d542533174946f25bc7f8277600d72f229123c
Camila Garrido <camila.t.garrido@gmail.com>
2024-03-07 21:45:04 -0300

    añadiendo print

--- dia1/mi_primer_programa.py
+++ dia1/mi_primer_programa.py
@@ -3,10 +3,29 @@ nombre = "Camila"
 apellido1="Garrido"
 apellido2="Barrientos"
 edad="25"
+año=2024 #es un numero
 #Python no permite sumar letras con numeros
+#suma de string = concatenar
 print("****** Informacion *********")
-print("**      "+nombre+"            **")
-print("**      "+apellido1+"             **")
-print("**      "+apellido2+"             **")
-print("**      "+edad+"                 **")
-print("****************************+**")
+print("Tu nombre es " + nombre )
+print("Tu apellido paterno es " + apellido1 )
+print("Tu apellido materno es " + apellido2 )
+print("tu tienes la edad de       " + edad )
+print("el año es",año,"mes",3,"dia",7)
+print("***************************")
+
+edad=18
+direccion='parcela 6'
+edad = 21.5 
+
+
+edad = "50"
+
+
+
+
+
+
+
+
+
