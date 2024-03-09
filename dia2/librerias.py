0733ebdb10226c05e1af338cb817b3543450c080
Camila Garrido <camila.t.garrido@gmail.com>
2024-03-08 21:41:41 -0300

    se añade comandos

--- dia2/librerias.py
+++ dia2/librerias.py
@@ -12,5 +12,14 @@ print(5%2) #1, modulo: el resto de la division
 print(os.getcwd()) #
 print(math.sqrt(9)) #me entrega la raiz cuadrada del numero ingresado y en forma de float
 #print(ceil(3.1416)) #importacion especifica de una libreria
-print(math.ceil(3.1416))
+print(math.ceil(3.1416)) #ceil aproxima al entero superior
+
+#pip
+#pip -V -> para saber la version 
+#python -m pip install --upgrade pip -> actualizar pip
+
+#pip install pandas
+#pip install pytorch (este no)
+#conda install pytorch
+#yes
 
