4c376de9f32cb810ec3d7d749a74a3bf00d1cc12
Camila Garrido <camila.t.garrido@gmail.com>
2024-03-15 22:15:20 -0300

    Se añade lower para convertir en minusculas las letras ingresadas en el juego cachipun

--- desafio2/cachipun.py
+++ desafio2/cachipun.py
@@ -5,21 +5,24 @@ import sys
 opciones_validas = ['piedra', 'papel', 'tijera']
 
 # Paso 2: Verificar si se proporcionó un argumento y si es válido
-if len(sys.argv) != 2 or sys.argv[1] not in opciones_validas:
+if len(sys.argv) != 2 or sys.argv[1].lower() not in opciones_validas:
     
-    # Paso 3: Mostrar un mensaje de error si el argumento es invalido
+    # Paso 3: Mostrar un mensaje de error si el argumento no es válido
     print("Argumento inválido. Debe ser piedra, papel o tijera.")
     sys.exit(1)
+else:
+    # Paso 4: Mostrar un mensaje de que los datos fueron ingresados correctamente
+    print("Datos ingresados correctamente!")
 
-# Paso 4: Obtener la elección del usuario y la elección aleatoria del pc 
-eleccion_usuario = sys.argv[1]
+# Paso 5: Obtener la elección del usuario y la elección aleatoria del computador
+eleccion_usuario = sys.argv[1].lower()  # Convertir a minúsculas
 eleccion_computadora = random.choice(opciones_validas)
 
-# Paso 5: Imprimir las elecciones del usuario y del pc 
+# Paso 6: Imprimir las elecciones del usuario y del computador
 print("El usuario escogió:", eleccion_usuario)
 print("La computadora escogió:", eleccion_computadora)
 
-# Paso 6: Determinar el resultado del juego
+# Paso 7: Determinar el resultado del juego
 if eleccion_usuario == eleccion_computadora:
     print("¡Es un empate!")
 elif (eleccion_usuario == 'piedra' and eleccion_computadora == 'tijera') or \
@@ -28,3 +31,6 @@ elif (eleccion_usuario == 'piedra' and eleccion_computadora == 'tijera') or \
     print("¡El usuario gana!")
 else:
     print("¡La computadora gana!")
+
+# Paso 8: Imprimir mensaje de fin del juego
+print("Fin del juego")
