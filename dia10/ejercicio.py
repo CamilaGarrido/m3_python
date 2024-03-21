e4bc64063c26e3fa7a782be3d7a47d41fe045568
Camila Garrido <camila.t.garrido@gmail.com>
2024-03-20 21:54:26 -0300

    se añaden cambios en el ejercicio de diccionario

--- dia10/ejercicio.py
+++ dia10/ejercicio.py
@@ -5,14 +5,23 @@ mi_diccionario = {
     "ciudad": "Concepción"
 }
 
+# Imprimir el diccionario original
+print(mi_diccionario)
+
 # Agregar un elemento
 mi_diccionario["ocupacion"] = "estudiante"
+
+# Imprimir el diccionario con el nuevo elemento
+print(mi_diccionario)
 
 # Cambiar un elemento
 mi_diccionario["edad"] = 26
 
+# Imprimir el diccionario con el cambio de edad
+print(mi_diccionario)
+
 # Eliminar un elemento
 del mi_diccionario["ciudad"]
 
-# Imprimir el diccionario actualizado
+# Imprimir el diccionario después de eliminar la ciudad
 print(mi_diccionario)
