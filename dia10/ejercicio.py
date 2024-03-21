5b8cf74cb4012f0a4f2b591ed061d4d06a61795e
Camila Garrido <camila.t.garrido@gmail.com>
2024-03-20 21:27:59 -0300

    ejercicio practico diccionario

---
+++ dia10/ejercicio.py
@@ -0,0 +1,18 @@
+# Crear un diccionario
+mi_diccionario = {
+    "nombre": "Camila",
+    "edad": 25,
+    "ciudad": "Concepción"
+}
+
+# Agregar un elemento
+mi_diccionario["ocupacion"] = "estudiante"
+
+# Cambiar un elemento
+mi_diccionario["edad"] = 26
+
+# Eliminar un elemento
+del mi_diccionario["ciudad"]
+
+# Imprimir el diccionario actualizado
+print(mi_diccionario)
